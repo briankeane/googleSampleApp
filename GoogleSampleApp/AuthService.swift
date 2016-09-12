07b1fa6743d07dbd9a0b9ced419a1c9935649085
briankeane <lonesomewhistle@gmail.com>
2016-09-12 16:11:57 -0500

    now stepping through to get all emails

--- GoogleSampleApp/AuthService.swift
+++ GoogleSampleApp/AuthService.swift
@@ -12,9 +12,7 @@ import CoreData
 import UIKit
 import Alamofire
 import PromiseKit
-
-
-let kLoggedOut = "loggedOut"
+import GoogleSignIn
 
 
 // -----------------------------------------------------------------------------
@@ -26,4 +24,82 @@ let kLoggedOut = "loggedOut"
 /// ----------------------------------------------------------------------------
 class AuthServiceClass
 {
+    func getChunkOfGoogleEmails(numberOfContacts:Int!, startIndex:Int!=1) -> Promise<Array<String>>
+    {
+        return Promise
+            {
+                (fulfill, reject) -> Void in
+                if let googleUser = GIDSignIn.sharedInstance().currentUser
+                {
+                    let accessToken = googleUser.authentication.accessToken
+                    let urlString = "https://www.google.com/m8/feeds/contacts/default/full?alt=json&max-results=\(numberOfContacts)&access_token=" + accessToken + "&start-index=\(startIndex)"
+                    print(urlString)
+                    Alamofire.request(.GET, urlString)
+                        .responseJSON
+                        {
+                            (response) -> Void in
+                            print("response")
+                            switch response.result
+                            {
+                            case .Success(let JSON):
+                                print("JSON")
+                                if let responseDict = JSON.objectForKey("feed") as? NSDictionary
+                                {
+                                    if let entries = responseDict["entry"] as? Array<Dictionary<String,AnyObject>>
+                                    {
+                                        var emails:Array<String> = Array()
+                                        for entry in entries
+                                        {
+                                            if let userContacts = entry as? Dictionary<String,AnyObject>
+                                            {
+                                                if let userEmails = userContacts["gd$email"] as? Array<Dictionary<String,AnyObject>>
+                                                {
+                                                    for userEmail in userEmails
+                                                    {
+                                                        if let address = userEmail["address"] as? String
+                                                        {
+                                                            emails.append(address)
+                                                        }
+                                                    }
+                                                }
+                                                
+                                            }
+                                        }
+                                        print("got \(emails.count) emails!")
+                                        if let links = responseDict.objectForKey("link") as? Array<Dictionary<String,AnyObject>>
+                                        {
+                                            for link in links
+                                            {
+                                                if ((link["rel"] as? String) == "next")
+                                                {
+                                                    self.getChunkOfGoogleEmails(numberOfContacts, startIndex: startIndex+numberOfContacts)
+                                                    .then
+                                                    {
+                                                        (newEmails) -> Void in
+                                                        fulfill(emails + newEmails)
+                                                    }
+                                                    return
+                                                }
+                                            }
+                                        }
+                                        fulfill(emails)
+                                    }
 
+                                }
+                                
+                                
+                                
+                                
+                            default: break
+                            }
+                    }
+                }
+                else
+                {
+                    print("no google user")
+                }
+        }
+    }
+}
+
+let AuthServiceInstance = AuthServiceClass()
--- GoogleSampleApp/ViewController.swift
+++ GoogleSampleApp/ViewController.swift
@@ -12,18 +12,39 @@ import GoogleSignIn
 class ViewController: UIViewController, GIDSignInUIDelegate {
 
     @IBOutlet weak var googleSignInButton: GIDSignInButton!
+    @IBOutlet weak var startingIndexTextField: UITextField!
+    @IBOutlet weak var numberToFetchTextField: UITextField!
     
-    override func viewDidLoad() {
+    var AuthService:AuthServiceClass! = AuthServiceInstance
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        
+        self.setupGoogleSignIn()
+    }
+    
+    func setupGoogleSignIn()
+    {
         GIDSignIn.sharedInstance().uiDelegate = self
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func getChunkOfEmailsButtonPressed(sender: AnyObject)
+    {
+        self.AuthService.getChunkOfGoogleEmails(Int(self.numberToFetchTextField.text!), startIndex: Int(self.startingIndexTextField.text!))
+        .then
+        {
+            (emails) -> Void in
+            print("ALL DONE -- WITH \(emails.count) emails")
+        }
+        
+    }
+    
 }
 
